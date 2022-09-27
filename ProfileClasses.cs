5782920c098f43116be122b4849a122980622387
Matas-Samsonas <94130181+Matas-Samsonas@users.noreply.github.com>
2022-09-27 23:33:49 +0300

    Creation of account class
    Prisijungimo informaciją perkeltą į naują Account klasę.

--- ProfileClasses.cs
+++ ProfileClasses.cs
@@ -2,16 +2,22 @@
 
 namespace ProfileClasses
 {
-    public class Profile
+    public class Account
     {
-        // public ProfileStatus { get; private set; } TODO: Sukurti ProfileStatus enum (Pvz. enum ProfileStatus { Client, Distributor })
-        
-        // public Kitchen[] Kitchens // For ProfileStatus == Client it is preferred kitchens to eat and for Distributors it is the Kitchens that they serve
+        // public AccountStatus { get; private set; } TODO: Sukurti ProfileStatus enum (Pvz. enum ProfileStatus { Client, Distributor })
         
         public string UserName { get; set; } // For ProfileStatus == Client it is a user created name and for Distributors - the name of their establishment
 
         public string Password { get; set; }
-
+        
+        public Account() {}
+    }
+    
+    public class Profile : Account
+    {
+        
+        // public Kitchen[] Kitchens // For ProfileStatus == Client it is preferred kitchens to eat and for Distributors it is the Kitchens that they serve
+        
         public string Email { get; set; }
 
         public string PhoneNumber { get; set; }
