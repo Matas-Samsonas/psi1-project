41f695b472e3f268ccfc9608893a77c0da292591
Matas-Samsonas <94130181+Matas-Samsonas@users.noreply.github.com>
2022-10-09 21:00:39 +0300

    Generic account data serializer
    Pakeičiau serializer, kad jis būtų generic, kad būtų galima jį naudoti su daugiau klasių. Dar pasiūlymas įdėjau interface, kuris turi property Uuid, nes pagalvojau, kad mums reikės serializuoti skelbimus ir kitą distributor data tarp kitų Json ir paieška bus vykdoma pagal Id tai gal būtų galima naudoti šį interface, kad supaprastintume paieška ir serializacija. Geriau būtų, nes galima interface įdėti kaip constraint generic ir taip search tiesiog tikrinti kad property egzistuos.

---
+++ PSI_MobileApp/PSI_MobileApp/IUsingUUID.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSI_MobileApp
+{
+    public interface IUsingUUID
+    {
+        public String Uuid { get; set; }
+    }
+}
