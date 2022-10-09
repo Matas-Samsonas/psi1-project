87f8b195f923d5240af314b60a91dda0c2a7628e
Matas-Samsonas <94130181+Matas-Samsonas@users.noreply.github.com>
2022-10-09 21:02:28 +0300

    Add files via upload

---
+++ PSI_MobileApp/PSI_MobileApp/AccountDataSerializer.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+// ReSharper disable StringCompareIsCultureSpecific.1
+namespace AccountDataSerializer;
+
+using ProfileClasses;
+using PSI_MobileApp;
+
+public class AccountDataSerializer<t> where t : class, IUsingUUID
+{
+    private string path;
+    private LinkedList<t> list;
+    public void Add(t newInstance)
+    {
+        list.AddLast(newInstance);
+        Reserialize();
+    }
+
+    private LinkedList<t> GetList()
+    {
+        if (!File.Exists(path))
+        {
+            using (File.Create(path)) { }
+        }
+
+        return JsonConvert.DeserializeObject<LinkedList<t>>(File.ReadAllText(path));
+    }
+
+    public t GetById(string id)
+    {
+        foreach (var instance in list)
+        {
+            if (string.Compare(instance.Uuid, id) == 0)
+            {
+                return instance;
+            }
+        }
+
+        return null;
+    }
+    public void Delete(t instanceToDelete)
+    {
+        list.Remove(instanceToDelete);
+        Reserialize();
+    }
+
+    public void Reserialize()
+    {
+        File.WriteAllText(path, JsonConvert.SerializeObject(list));
+    }
+
+    public AccountDataSerializer (string path)
+    {
+        this.path = path;
+        this.list = GetList();
+    }
+}
