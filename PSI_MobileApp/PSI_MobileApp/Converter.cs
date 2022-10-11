de127502584e6777635bb4cf7c589e8fb59951a6
Matas-Samsonas <94130181+Matas-Samsonas@users.noreply.github.com>
2022-10-11 21:46:54 +0300

    Added array to string converter
    Extension metodas kuris array konvertuoją į string.

---
+++ PSI_MobileApp/PSI_MobileApp/Converter.cs
@@ -0,0 +1,21 @@
+namespace PSI_MobileApp;
+
+public static class Converter
+{
+    public static string ArrayToString(this Array array)
+    {
+        string newString = "";
+        foreach (var member in array)
+        {
+            if (newString != String.Empty)
+            {
+                newString += ", ";
+            }
+
+            newString += member.ToString();
+        }
+
+        newString += ".";
+        return newString;
+    }
+}
