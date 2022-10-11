ae28adedf9dc1ee68f0314c9467797af794d548b
Matas-Samsonas <94130181+Matas-Samsonas@users.noreply.github.com>
2022-10-11 20:22:57 +0300

    Removed [ObservableProperty] iš Address

--- PSI_MobileApp/PSI_MobileApp/Address.cs
+++ PSI_MobileApp/PSI_MobileApp/Address.cs
@@ -2,12 +2,9 @@ namespace PSI_MobileApp;
 
 public struct Address
 {
-    [ObservableProperty] 
     private string city;
 
-    [ObservableProperty] 
     private string streetName;
-
-    [ObservableProperty] 
+    
     private int streetNumber;
 }
