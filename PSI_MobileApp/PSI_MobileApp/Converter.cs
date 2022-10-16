55706a8970efa78fdfd121600e465dcf9b103550
Matas-Samsonas <94130181+Matas-Samsonas@users.noreply.github.com>
2022-10-16 11:30:02 +0300

    Enum Array to string converter
    Perrašiau converter, kad nebūtų iš enum į string, o enum array į string, nes taip parašius xaml paprastai konvertuojasi. 
    
    <Label Text="{Binding Tags, Converter={StaticResource arrayToString}}" Grid.Column="4" />

--- PSI_MobileApp/PSI_MobileApp/Converter.cs
+++ PSI_MobileApp/PSI_MobileApp/Converter.cs
@@ -2,14 +2,21 @@ using System.Globalization;
 
 namespace PSI_MobileApp;
 
-public class Converter
+public class Converter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        string EnumString;
+        string EnumString = "";
         try
         {
-            EnumString = Enum.GetName(value.GetType(), value);
+            foreach (var item in (Array)value)
+            {
+                if (EnumString != "")
+                {
+                    EnumString += " ";
+                }
+                EnumString += item.ToString();
+            }
             return EnumString;
         }
         catch
@@ -17,7 +24,7 @@ public class Converter
             return String.Empty;
         }
     }
-    
+
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
         throw new NotImplementedException();
